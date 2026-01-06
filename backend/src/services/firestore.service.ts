1b7c74dc8fd600af6949353774b8cf34e5162cdf
Jesus Abraham Salgado <forever_red_9@hotmail.com>
2026-01-06 02:39:12 -0800

    Remove firebase

--- backend/src/services/firestore.service.ts
+++
@@ -1,8 +0,0 @@
-// Deprecated: Using PostgreSQL instead of Firestore
-
-export function collection(name: string) {
-  throw new Error('Firestore service is deprecated. Use PostgreSQL queries instead.');
-}
-
-export default { collection };
-
